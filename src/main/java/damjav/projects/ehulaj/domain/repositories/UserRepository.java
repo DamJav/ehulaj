e629ed50d11a791a5d7422a6e3f388e8be8e71e5
Damian <dga1999@wp.pl>
2019-12-12 12:29:10 +0100

    Admin can lock/unlock users

--- src/main/java/damjav/projects/ehulaj/domain/repositories/UserRepository.java
+++ src/main/java/damjav/projects/ehulaj/domain/repositories/UserRepository.java
@@ -2,9 +2,22 @@ package damjav.projects.ehulaj.domain.repositories;
 
 import damjav.projects.ehulaj.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE users SET active = false WHERE id = ?",nativeQuery = true)
+    void lockUserByUserId(Long id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE users SET active = true WHERE id = ?",nativeQuery = true)
+    void unlockUserByUserId(Long id);
 
 }
