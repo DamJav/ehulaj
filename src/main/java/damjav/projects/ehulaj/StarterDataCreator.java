819e4b23de037ba00274ac09dea3facbd4a23b62
Damian <dga1999@wp.pl>
2019-12-13 09:57:27 +0100

    Start pack

---
+++ src/main/java/damjav/projects/ehulaj/StarterDataCreator.java
@@ -0,0 +1,35 @@
+package damjav.projects.ehulaj;
+
+
+import damjav.projects.ehulaj.domain.entities.Role;
+import damjav.projects.ehulaj.domain.repositories.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Profile("heroku") @Slf4j @RequiredArgsConstructor
+public class StarterDataCreator implements ApplicationRunner {
+
+    private final RoleRepository roleRepository;
+
+    @Override @Transactional
+    public void run(ApplicationArguments args) throws Exception {
+        createRoles();
+    }
+
+    private void createRoles() {
+        Role userRole = new Role();
+        userRole.setName("ROLE_USER");
+        roleRepository.save(userRole);
+
+        Role adminRole = new Role();
+        adminRole.setName("ROLE_ADMIN");
+        roleRepository.save(adminRole);
+    }
+}
--- src/main/java/damjav/projects/ehulaj/domain/entities/Role.java
+++ src/main/java/damjav/projects/ehulaj/domain/entities/Role.java
@@ -16,6 +16,7 @@ import javax.persistence.*;
 public class Role {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false, unique = true)
     private String name;
